dd9867d11150a9fa274aa3c85a07f24d5b7d84ce
unknown <alamgirhossaindrubo@gmail.com>
2025-12-18 00:00:49 +0600

    fh

---
+++ D_Anagram.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define cyes cout<<"Yes"<<endl;
+#define cno cout<<"No"<<endl;
+#define all(x) x.begin(), x.end()
+#define choto(x) sort((x).begin(), (x).end())
+#define boro(x) sort((x).rbegin(), (x).rend())
+#define nl cout<<endl;
+#define endl '\n'
+#define love int main()
+#define vtr vector
+#define pii pair<int,int>
+#define out(x) cout << x << '\n';
+#define out2(x,y) cout << x << " " << y << '\n';
+#define vii vector<int> 
+#define vll vector<ll>
+#define imax INT_MAX
+#define imin INT_MIN
+#define pb push_back
+#define vsum(x) accumulate((x).begin(), (x).end(), 0LL)
+#define fix(n, val) fixed << setprecision(n) << val
+#define Alamgir ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+using namespace std;
+//Constants
+const long double pi= 3.141592653589793238;
+const ll MOD=1e9+7;
+
+//checks
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);} //__gcd 
+ll lcm(ll a, ll b){return (a/gcd(a,b)*b);}
+bool isPrime(ll n){if(n<=1)return false;if(n<=3)return true;if(n%2==0||n%3==0)return false;for(int i=5;i*i<=n;i=i+6)if(n%i==0||n%(i+2)==0)return false;return true;}
+
+void solve(){
+    queue<int> q;
+    q.push(10);
+    q.emplace(20);
+    q.push(10);
+    while(!q.empty()){
+        cout<<q.front()<<" ";
+        q.pop();
+    }
+}
+love{
+    Alamgir
+    int t=1; 
+    // cin>>t;
+    for(int i=1;i<=t;i++){
+        solve();
+    }
+    return 0;    
+}
--- tmp.cpp
+++ tmp.cpp
@@ -1,8 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    cout << (-7324 % 100) << endl;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        int ans = INT_MAX;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                int tmp = a[i] + a[j] + (j - i);
+                ans = min(ans, tmp);
+            }
+        }
+        cout << ans << endl;
+    }
     return 0;
 }
